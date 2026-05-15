63fa1672a54754af534ae71ed29e0482316c9616
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-15 11:50:35 -0400

    test

---
+++ src/main/java/entrenador/entrenador/WebClient/ClienteClient.java
@@ -0,0 +1,31 @@
+package entrenador.entrenador.WebClient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@Component
+public class ClienteClient {
+    private final WebClient webClient;
+
+    public ClienteClient(@Value("${cliente-service.url}")String clienteServidor){
+        this.webClient = WebClient.builder().baseUrl(clienteServidor).build();
+    }
+
+    //metodo para poder comunicarme con el microservicio
+    //cliente y verificar si el cliente existe
+    //devolver una estructura de mapa que representa el json
+    public Map<String, Object> obtenerClienteId(Long id){
+        //realizar una consulta HTTP de tipo get al microservicio cliente
+        return this.webClient.get()
+                .uri("/{id}", id)
+                .retrieve()
+                .onStatus(status -> status.is4xxClientError(),
+                        response -> response.bodyToMono(String.class)
+                                .map(body -> new RuntimeException("Cliente no encontrado")))
+                .bodyToMono(Map.class).block();
+
+    }
+}
