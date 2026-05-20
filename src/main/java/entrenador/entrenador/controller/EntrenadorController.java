bebb05a53e30f3be49c864dc33bf83513f52a48f
Crizz12 <cri.cifuentesc@duocuc.cl>
2026-05-19 23:02:54 -0400

    cambio request

--- src/main/java/entrenador/entrenador/controller/EntrenadorController.java
+++ src/main/java/entrenador/entrenador/controller/EntrenadorController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/entrenadores")
+@RequestMapping("/v1/entrenadores")
 public class EntrenadorController {
     @Autowired
     private EntrenadorService entrenadorService;
