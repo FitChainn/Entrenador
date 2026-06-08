7c4218139d3954d8ea542ae81469e9d01ba9e410
neurott <neurott@protonmail.com>
2026-06-08 10:31:32 -0400

    Openapicfg

---
+++ src/main/java/entrenador/entrenador/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package entrenador.entrenador.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Entrenador API - FitChain")
+                        .description("API DE GESTIÓN DEL ENTRENADOR")
+                        .version("1.0.0")).servers(List.of(
+                        new Server().url("http://localhost:8082").description("Local")
+                ));
+    }
+}
